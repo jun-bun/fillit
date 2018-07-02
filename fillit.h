5da4a8ab4f2706e809f6c4ed63ee22bfd9f3339e
juwong <juwong@42.US.ORG>
2018-07-02 16:18:52 -0700

    update fillit.h

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: juwong <juwong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/29 18:16:49 by dkotov            #+#    #+#             */
-/*   Updated: 2018/07/01 21:57:42 by juwong           ###   ########.fr       */
+/*   Updated: 2018/07/02 16:15:14 by juwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,12 @@ typedef struct	s_piece
 	struct s_piece		*next;
 }				t_piece;
 
+void	ft_print_board(char **board);
+void	solve(t_list *pieces);
 int		*piece_cords(int piece);
 char	**piece_create(int	piece);
 char	**make_board(size_t size);
+size_t	get_min_board_size(t_list *pieces);
 int		*get_boundry_piece(char *piece);
 int		check_valid_space(t_list *piece, char ***board, int x, int y);
 int		put_piece(t_list *piece, char ***board);
