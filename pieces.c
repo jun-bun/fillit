0402d6c8dd06776dfb6318ede4dbac7b2caba607
juwong <juwong@42.US.ORG>
2018-07-05 15:28:15 -0700

    solved

--- pieces.c
+++ pieces.c
@@ -6,69 +6,136 @@
 /*   By: juwong <juwong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/29 19:51:07 by juwong            #+#    #+#             */
-/*   Updated: 2018/07/01 20:17:15 by juwong           ###   ########.fr       */
+/*   Updated: 2018/07/05 13:26:11 by juwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-/*
-** make_piece(1) -> {{###},
-**					 {..#}}
-**
-**{#.}
-**{#.}
-**{##}
-**{#}
-**{#}
-**{#}
-**{#}
-**{####}
-char **make_piece(int piece, int	piece_num)
-*/
-
-/*
-** Might go in a different direction and not need this code
-int 	*piece_cords(int piece)
+int		*get_boundry_piece(char *piece)
 {
-	int		s[2][8] = {{0,0,0,1,1,0,1,1},{0,0,0,1,0,2,0,3}};
-
-//s = (int**)malloc(sizeof(int*) * 2);
-//*s = (int*)malloc(sizeof(int) * 16);
-//s[0] = {0,0,0,1,1,0,1,1};
-//s[1] = {0,0,0,1,0,2,0,3};
-if (!piece)
-	return s[piece];
-else
-	return (NULL);
-}
-
-char	**piece_create(int	piece)
-{
-	int		*cords;
-	char	**piece_array;
 	int		i;
+	int		j;
+	int		max_i;
+	int		*boundry;
 
-	if (!piece)
-		return NULL;
-	cords = piece_cords(piece);
 	i = 0;
-	if (!(piece_array = (char**)malloc(sizeof(char*) * cords[6] + 1)))
-		return (NULL);
-	while(i <= cords[6])
+	j = 1;
+	max_i = 0;
+	boundry = (int *)malloc(sizeof(int) * 2);
+	while (*piece)
 	{
-		if (!(piece_array[i] = ft_strnew(cords[7])))
-			return (NULL);
-		ft_strset(piece_array[i], '.');
-		i++;
+		if (*piece == '\n')
+		{
+			j++;
+			max_i = max_i < i ? i : max_i;
+			i = 0;
+		}
+		else
+			i++;
+		piece++;
 	}
-	i = 0;
-	while (i < 8)
-	{
-		piece_array[cords[i]][cords[i + 1]] = '#';
-		i = i + 2;
-	}
-	piece_array[cords[i]] = NULL;
-	return (piece_array);
+	boundry[0] = max_i;
+	boundry[1] = j;
+	return (boundry);
 }
-*/
+
+void remove_piece(char ***board)
+ {
+	char	**ptr_board;
+	char	last_piece;
+	int		i;
+	int		k;
+
+	ptr_board = *board;
+	last_piece = 'A';
+	k = 0;
+	while (ptr_board[k])
+	{
+		i = 0;
+		while (ptr_board[k][i])
+		{
+			if (ft_isalpha(ptr_board[k][i]))
+			{
+				if (last_piece < ptr_board[k][i])
+					last_piece = ptr_board[k][i];
+			}
+			i++;
+		}
+		k++;
+	}
+	k = 0;
+	while (ptr_board[k])
+	{
+		i = 0;
+		while (ptr_board[k][i])
+		{
+			if (ptr_board[k][i] == last_piece)
+				ptr_board[k][i] = '.';
+			i++;
+		}
+		k++;
+	}
+ }
+
+int		check_valid_space(char *piece, char ***board, int x, int y)
+{
+	char	**board2;
+	char	*ptr;
+	int		i;
+	int		j;
+
+	if (!(piece))
+		return (0);
+	board2 = *board;
+	ptr = piece;
+	i = 0;
+	j = 0;
+	while (*ptr)
+	{
+		if (ft_isalpha(*ptr))
+		{
+			if (board2[j + y][i + x] == '.')
+				i++;
+			else
+				return (0);
+		}
+		else if (*ptr == '.')
+			i++;
+		else if (*ptr == '\n')
+		{
+			j++;
+			i = 0;
+		}
+		ptr++;
+	}
+	return (1);
+}
+
+int		put_piece(char *piece, char ***board, t_point p)
+{
+	char	**board2;
+	int		k;
+
+	board2 = *board;
+	if (!piece)
+		return (0);
+	k = p.x;
+	while (*(char*)piece)
+	{
+		if (ft_isalpha(*(char*)piece))
+		{
+			board2[p.y][k] = *(char*)piece;
+			k++;
+		}
+		else if (*((char*)piece) == '.')
+			k++;
+		else if (*((char*)piece) == '\n')
+		{
+			p.y++;
+			k = p.x;
+		}
+		piece++;
+	}
+	return (1);
+}
--- solve.c
+++ solve.c
@@ -6,41 +6,15 @@
 /*   By: juwong <juwong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/02 14:43:53 by juwong            #+#    #+#             */
-/*   Updated: 2018/07/05 13:14:48 by juwong           ###   ########.fr       */
+/*   Updated: 2018/07/05 15:27:47 by juwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-/*
-void	solve(t_list *pieces)
-{
-	char	**board;
-	size_t	size;
-	int		p.x;
-	int		p.y;
-
-    size = get_min_board_size(pieces);
-    board = make_board(size);
-	
-//	p.x = 0;
-//	p.y = 0;
-	if (check_valid_space(piece, board, i, j)
-		return ;
-
-	ft_print_board(board);
-	ft_putchar('\n');
-	pieces = pieces->next;
-
-}
-*/
-
 int		solve(t_piece 	*l_pieces, char ***board, int size, t_point p)
 {
 	int			*boundry;
-	//int		res;
-	//t_list	*new_l;
-	//int		res;
 	t_point		np;
 	int			res;
 
@@ -51,16 +25,6 @@ int		solve(t_piece 	*l_pieces, char ***board, int size, t_point p)
 	np.y = 0;
 	while (p.x + boundry[0] <= size && p.y + boundry[1] <= size)
 	{
-		/*
-		if ((!piece_placeable(l_pieces->content, board, size, p)))
-		{
-			p.x++;
-			new_l = ft_lstnew(remove_piece(board), 8);
-			ft_lstadd(&l_pieces, new_l);
-			//ft_putstr(new_l->content);
-			res = (solve(new_l, board, size, p));
-		}
-		*/
 		if (check_valid_space(l_pieces->content, board, p.x, p.y))
 		{
 			put_piece(l_pieces->content, board, p);
@@ -112,71 +76,3 @@ void	start_solve(t_piece *l_pieces)
 	ft_putendl("Found answer!");
 	ft_print_board(board);
 }
-/*
-	while (pieces)
-	{
-//		put_piece(pieces->content, &board, size, p);
-		ft_print_board(board);
-		while (!(put_piece(pieces->content, &board, size, p)))
-		{
-			remove_piece(&board);
-			pieces = l_last;
-			p.x++;
-			if (p.x > (int)size)
-			{
-				p.x = 0;
-				p.y++;
-			}
-		p.x = 0;
-		p.y = 0;
-		l_last = pieces;
-		if (pieces->next)
-			pieces = pieces->next;
-		else
-			return ;
-		}
-
-	}
-*/
-
-/*
-void	solve(t_list *pieces)
-{
-	char	**board;
-	size_t	size;
-	int		p.x;
-	int		p.y;
-
-    size = get_min_board_size(pieces);
-    board = make_board(size);
-	
-	p.x = 0;
-	p.y = 0;
-	while(pieces)
-	{
-		if (!(check_valid_space(pieces, &board, p.x, p.y)))
-		{
-			ft_print_board(board);
-			ft_putchar('\n');
-			remove_piece(&board);
-			ft_print_board(board);
-			p.x++;
-			if (p.x > (int)size)
-			{
-				p.x = 0;
-				p.y++;
-			}
-		}
-		else
-		{
-			put_piece(pieces, &board, p.x, p.y);
-			pieces = pieces->next;
-			p.x = 0;
-			p.y = 0;
-		}
-	}
-	
-	remove_piece(&board);
-	ft_print_board(board);
-}
-*/
