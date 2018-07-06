da4c8b9371a392d28c4998bf7d848c69b8e0ec58
Dmitry Kotov <dkotov@e1z3r6p4.42.us.org>
2018-07-05 21:28:11 -0700

    almost normed fillit.h

--- fillit.h
+++ fillit.h
@@ -15,7 +15,7 @@
 # define FILLIT_H
 
 # include "libft/libft.h"
-#include <stdio.h>
+# include <stdio.h>
 
 typedef struct	s_point
 {
@@ -43,17 +43,17 @@ int		check_valid_space(char *piece, char ***board, int x, int y);
 int		piece_placeable(char *piece, char ***board, int size, t_point p);
 int		put_piece(char *piece, char ***board, t_point p);
 void	remove_piece(char ***board, char s);
-int	valid_tetro_chars(char *s, int start, int end);
-int	valid_tetro_map(char *s);
-int	valid_tetro_connections(char *s, int start, int end, int pos);
-int valid_tetro_connections_checker(char *s, int start, int end);
+int		valid_tetro_chars(char *s, int start, int end);
+int		valid_tetro_map(char *s);
+int		valid_tetro_connections(char *s, int start, int end, int pos);
+int		valid_tetro_connections_checker(char *s, int start, int end);
 char	tetro_symbol(int tetro_num);
-int	*tetro_next_coordinate(char *s, int start, int skip, int tetro_num);
-int	**tetro_get_coordinates(char *s, int start, int end, int tetro_num);
-int	**tetro_clean_coordinates(int **arr);
+int		*tetro_next_coordinate(char *s, int start, int skip, int tetro_num);
+int		**tetro_get_coordinates(char *s, int start, int end, int tetro_num);
+int		**tetro_clean_coordinates(int **arr);
 char	*tetro_new_string(int **arr);
 t_piece *struct_new_piece(char *s, int tetro_num);
 t_piece *struct_make_list(t_piece *first_piece, char *s, int tetro_num);
-void print_t_piece(t_piece *pieces);
+void	print_t_piece(t_piece *pieces);
 
 #endif
